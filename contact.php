50a4fefffce33f8259145b03f8d6b51766a99b85
Bibek kun <bibekrai655@gmail.com>
2022-07-12 13:34:37 +0545

    added contact section

---
+++ contact.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html>
+<head>
+		
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+
+		<title>Nalanda Tripitak | Medical care provider | Contact</title>	
+
+        <meta name="keywords" content="Nalanda Tripitak | Contact" />
+		<meta name="description" content="Health Care Center | Contact">
+		<meta name="author" content="nalandatripitak">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+		<?php include('assets/partials/header.php'); ?>
+		
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-SEP1T05Z5V"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'G-SEP1T05Z5V');
+		</script>
+
+	</head>
+	<body>
+
+		<div class="body">
+
+			<div role="main" class="main">
+				
+				<section class="page-header page-header-modern bg-color-primary page-header-md">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-8 order-2 order-md-1 align-self-center p-static">
+								<h1>Contact</h1>
+								<span class="sub-title">Send us a message or call us for more information</span>
+							</div>
+							<div class="col-md-4 order-1 order-md-2 align-self-center">
+								<ul class="breadcrumb d-block text-md-end breadcrumb-light">
+									<li><a href="index.php">Home</a></li>
+									<li class="active">Contact</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<div class="container">
+
+					<div class="row mt-5">
+						<div class="col-lg-4">
+							<div class="feature-box feature-box-style-2">
+								<div class="feature-box-icon pt-0">
+									<i class="icon-location-pin icons"></i>
+								</div>
+								<div class="feature-box-info">
+									<h4 class="mb-2">Address</h4>
+									<p class="text-4">
+										Balaju
+										Kathmandu, Nepal
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="feature-box feature-box-style-2">
+								<div class="feature-box-icon pt-0">
+									<i class="icon-phone icons"></i>
+								</div>
+								<div class="feature-box-info">
+									<h4 class="mb-2">Phone</h4>
+									<p class="text-4">
+										9815237349 <br>
+										9834791240
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="feature-box feature-box-style-2">
+								<div class="feature-box-icon pt-0">
+									<i class="icon-envelope icons"></i>
+								</div>
+								<div class="feature-box-info">
+									<h4 class="mb-2">Email</h4>
+									<p class="text-4">
+										<a href="https://www.okler.net/cdn-cgi/l/email-protection#91fcf0f8fdd1f4e9f0fce1fdf4bff2fefc" class="text-decoration-none"><span class="__cf_email__" data-cfemail="6a070b03062a0f120b071a060f44090507">@infonalandatritak.com</span></a><br>
+										</a>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<hr class="solid">
+
+					<div class="row pt-4 mb-4">
+						<div class="col-lg-6">
+							<div id="googlemaps" class="google-map m-0 mb-5"></div>
+						</div>
+						<div class="col-lg-6">
+
+							<h4 class="font-weight-semibold mb-4">Send a Message to Us</h4>
+
+							<form class="contact-form" action="https://www.okler.net/previews/porto/9.4.0/php/contact-form.php" method="POST">
+								<div class="contact-form-success alert alert-success d-none mt-4">
+									<strong>Success!</strong> Your message has been sent to us.
+								</div>
+
+								<div class="contact-form-error alert alert-danger d-none mt-4">
+									<strong>Error!</strong> There was an error sending your message.
+									<span class="mail-error-message text-1 d-block"></span>
+								</div>
+								<div class="row">
+									<div class="form-group col">
+										<input type="text" placeholder="Your Name" value="" data-msg-required="Please enter your name." maxlength="100" class="form-control" name="name" required>
+									</div>
+								</div>
+								<div class="row">
+									<div class="form-group col">
+										<input type="email" placeholder="Your E-mail" value="" data-msg-required="Please enter your email address." data-msg-email="Please enter a valid email address." maxlength="100" class="form-control" name="email" required>
+									</div>
+								</div>
+								<div class="row">
+									<div class="form-group col">
+										<input type="text" placeholder="Subject" value="" data-msg-required="Please enter the subject." maxlength="100" class="form-control" name="subject" required>
+									</div>
+								</div>
+								<div class="row">
+									<div class="form-group col">
+										<textarea maxlength="5000" placeholder="Message" data-msg-required="Please enter your message." rows="5" class="form-control" name="message" required></textarea>
+									</div>
+								</div>
+								<div class="row">
+									<div class="form-group col">
+										<input type="submit" value="Send Message" class="btn btn-primary btn-lg mb-5" data-loading-text="Loading...">
+									</div>
+								</div>
+							</form>
+
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+
+			
+		</div>
+		<?php include('assets/partials/footer.php'); ?>
+		<!-- Vendor -->
+		<script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/jquery.appear/jquery.appear.min.js"></script>
+		<script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+		<script src="vendor/jquery.cookie/jquery.cookie.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<script src="vendor/jquery.validation/jquery.validate.min.js"></script>
+		<script src="vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+		<script src="vendor/jquery.gmap/jquery.gmap.min.js"></script>
+		<script src="vendor/lazysizes/lazysizes.min.js"></script>
+		<script src="vendor/isotope/jquery.isotope.min.js"></script>
+		<script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+		<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<script src="vendor/vide/jquery.vide.min.js"></script>
+		<script src="vendor/vivus/vivus.min.js"></script>
+
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+
+		<!-- Current Page Vendor and Views -->
+		<script src="js/views/view.contact.js"></script>
+
+		<!-- Demo -->
+		<script src="js/demos/demo-medical.js"></script>
+
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+	<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"6dcb4d80de9d845f","version":"2021.12.0","r":1,"token":"03fa3b9eb60b49789931c4694c153f9b","si":100}' crossorigin="anonymous"></script>
+</body>
+</html>
