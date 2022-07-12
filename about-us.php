177ea324e75d8be29c4b7ca9340ba991ed21a1d2
Bibekkun <bibek@infiniteitsolutionsnepal.com>
2022-07-12 11:08:52 +0545

    made changes in about-us section

---
+++ about-us.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html>
+<head>
+		
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+
+		<title>Nalanda Tripitak | Medical care provider | About us</title>	
+
+		<meta name="keywords" content="Nalanda Tripitak | About us" />
+		<meta name="description" content="Health Care Center | About us">
+		<meta name="author" content="nalandatripitak">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+		<?php include('assets/partials/header.php'); ?>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-SEP1T05Z5V"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'G-SEP1T05Z5V');
+		</script>
+
+	</head>
+	<body>
+
+		<div class="body">
+
+			
+			<div role="main" class="main">
+				
+				<section class="page-header page-header-modern bg-color-primary page-header-md">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-8 order-2 order-md-1 align-self-center p-static">
+								<h1>About Us</h1>
+								<span class="sub-title">20 Years Caring About You</span>
+							</div>
+							<div class="col-md-4 order-1 order-md-2 align-self-center">
+								<ul class="breadcrumb d-block text-md-end breadcrumb-light">
+									<li><a href="index.php">Home</a></li>
+									<li><a>About Us</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<div class="container">
+
+					<div class="row mt-5">
+						<div class="col-lg-8">
+							<h2 class="font-weight-semibold mb-3">Welcome to Nalanda Tripitak Health Care Centre</h2>
+
+							<p class="lead font-weight-normal">We are a well-equipped multispecialty health care center which aims at providing you personalized care with a touch of empathy that we have learnt serving patients from all around the world over a period of 20 years in various avatars.<br><p class="lead font-weight-normal">Over the years we have come to an understanding that patient care involves as much modern equipment’s as it requires caring human touch. This learning is applied throughout our vision and policies</p>
+
+							<p class="lead font-weight-normal">We were established as a fully functioning hospital with experience in healthcare. Our health care center boast of over 12 consultants with over 20 years of experience combined in treating patients. We have a 24*7 emergency team with special training in dealing with trauma and a fully automated laboratory and radiology unit.
+
+							Our operating team have over the years dealt with more than 500 minor and major procedures with help of most advanced equipment’s including <br>C-arm and lithotripter.
+
+							Visit us to get more than just world class doctors and equipment’s for we know that you deserve more.</p>
+
+							<p class="lead font-weight-normal">Nalanda Tripitak Health Care Center</p>
+
+							
+						</div>
+						<div class="col-lg-4">
+
+							<div class="owl-carousel owl-theme dots-inside mt-2" data-plugin-options="{'items': 1, 'margin': 10, 'animateOut': 'fadeOut', 'autoplay': true, 'autoplayTimeout': 3000}">
+								<div>
+									<img src="img/demos/medical/gallery/gallery-2.jpg" alt="" class="img-fluid box-shadow-custom" /> 
+								</div>
+								<div>
+									<img src="img/demos/medical/gallery/gallery-3.jpg" alt="" class="img-fluid box-shadow-custom" /> 
+								</div>
+								<div>
+									<img src="img/demos/medical/gallery/g-4.jpg" alt="" class="img-fluid box-shadow-custom" /> 
+								</div>
+							</div>
+
+							<div class="row mt-5 mt-lg-4 mb-4 counters">
+								<div class="col-lg-12 mb-4">
+									<div class="counter counter-primary">
+										<strong data-to="20">0</strong>
+										<label>Years in Business</label>
+									</div>
+								</div>
+								<div class="col-lg-12 mb-4">
+									<div class="counter counter-primary">
+										<strong data-to="6">0</strong>
+										<label>Departments</label>
+									</div>
+								</div>
+							</div>
+
+						</div>
+					</div>
+
+					<div class="row mb-5">
+						<div class="col">
+							<div class="toggle toggle-primary toggle-simple" data-plugin-toggle>
+								<section class="toggle">
+									<a class="toggle-title">Payment Options</a>
+									<div class="toggle-content">
+										<p class="lead">Payment System</p>
+										<ul>
+											<li>Cash</li>
+											<li>Online payment
+												<ul>
+													<li>Bank Account: 06538734859389</li>
+													<li>E-sewa: 9825070932</li>
+												</ul>
+											</li>
+										</ul>
+									</div>
+								</section>
+								<section class="toggle">
+									<a class="toggle-title">Appointments</a>
+									<div class="toggle-content">
+										<p class="lead font-weight-normal"><a href="doctors.php">Get appointment from here</p>
+									</div>
+								</section>
+								<section class="toggle">
+									<a class="toggle-title">Ambulance Service</a>
+									<div class="toggle-content">
+										<p class="lead font-weight-normal">Phone number</p>
+										<ul>
+											<li>9815237349</li>
+											<li>9834791240</li>
+										</ul>
+									</div>
+								</section>
+
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+				<section class="call-to-action call-to-action-default call-to-action-big content-align-center mb-0 mt-3">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-9 col-lg-9">
+								<div class="call-to-action-content">
+									<h3>"Health is the most important thing you have in life!"</h3>
+									<p class="mb-0">Contact us now to schedule an appointment.</p>
+								</div>
+							</div>
+							<div class="col-sm-3 col-lg-3">
+								<div class="call-to-action-btn">
+									<a href="contact.php"  class="btn btn-lg btn-primary">Contact Us</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+		</div>
+		<?php include('assets/partials/footer.php'); ?>
+
+		<!-- Vendor -->
+		<script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/jquery.appear/jquery.appear.min.js"></script>
+		<script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+		<script src="vendor/jquery.cookie/jquery.cookie.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<script src="vendor/jquery.validation/jquery.validate.min.js"></script>
+		<script src="vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+		<script src="vendor/jquery.gmap/jquery.gmap.min.js"></script>
+		<script src="vendor/lazysizes/lazysizes.min.js"></script>
+		<script src="vendor/isotope/jquery.isotope.min.js"></script>
+		<script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+		<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<script src="vendor/vide/jquery.vide.min.js"></script>
+		<script src="vendor/vivus/vivus.min.js"></script>
+
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+
+		<!-- Current Page Vendor and Views -->
+		<script src="js/views/view.contact.js"></script>
+
+		<!-- Demo -->
+		<script src="js/demos/demo-medical.js"></script>
+
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+	<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"6dcb4d539ce3845f","version":"2021.12.0","r":1,"token":"03fa3b9eb60b49789931c4694c153f9b","si":100}' crossorigin="anonymous"></script>
+</body>
+</html>
