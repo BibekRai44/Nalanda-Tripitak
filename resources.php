b44cb3f2d5b780003a82698e995dd110e1090098
Bibek kun <bibekrai655@gmail.com>
2022-07-12 13:24:57 +0545

    added resources section

---
+++ resources.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+		
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+
+		<title>Nalanda Tripitak | Medical care provider | Departments | Resources</title>	
+
+		<meta name="keywords" content="Nalanda Tripitak | Resources" />
+		<meta name="description" content="Health Care Center | Resources">
+		<meta name="author" content="nalandatripitak">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+		<?php include('assets/partials/header.php'); ?>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-SEP1T05Z5V"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'G-SEP1T05Z5V');
+		</script>
+
+	</head>
+	<body>
+
+		<div class="body">
+
+			<div role="main" class="main">
+				
+				<section class="page-header page-header-modern bg-color-primary page-header-md">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-8 order-2 order-md-1 align-self-center p-static">
+								<h1>Resources</h1>
+								<span class="sub-title">Everything you need to know</span>
+							</div>
+							<div class="col-md-4 order-1 order-md-2 align-self-center">
+								<ul class="breadcrumb d-block text-md-end breadcrumb-light">
+									<li><a href="index.php">Home</a></li>
+									<li class="active">Resources</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<div class="container">
+
+					<div class="row mt-5 mb-5">
+						<div class="col">
+							<h2 class="font-weight-semibold mb-3">Patient Resources</h2>
+
+							<p class="lead font-weight-normal">We Nalanda Health Care Center Help patients get back to care and improve health outcomes.</p>
+							<div class="row mt-4">
+								<div class="col-md-4 mb-3 mb-md-0">
+									<a href="prepare-for-visit.php" class="text-decoration-none">
+										<img alt="" class="img-fluid" src="img/demos/medical/gallery/gallery-2.jpg">
+									</a>
+								</div>
+								<div class="col-md-8">
+									<h4 class="font-weight-semibold mb-1 mt-1">
+										<a href="prepare-for-visit.php" class="text-decoration-none">Prepare for visit</a>
+									</h4>
+									<p class="mb-3">Visit our Health Care Center</p>
+									<p><a href="prepare-for-visit.php" class="btn btn-outline btn-quaternary custom-button text-uppercase mt-2 mb-3 font-weight-bold btn-md text-1">view more...</a></p>
+								</div>
+							</div>
+
+							<div class="row mt-4">
+								<div class="col-md-4 mb-3 mb-md-0">
+									<a href="prepare-for-visit.php" class="text-decoration-none">
+										<img alt="" class="img-fluid" src="img/demos/medical/gallery/gallery-3.jpg">
+									</a>
+								</div>
+								<div class="col-md-8">
+									<h4 class="font-weight-semibold mb-1 mt-1">
+										<a href="surgery-process.php" class="text-decoration-none">Surgery proccess</a>
+									</h4>
+									<p class="mb-3">Watch our process of surgery operationing</p>
+									<p><a href="surgery-process.php" class="btn btn-outline btn-quaternary custom-button text-uppercase mt-2 mb-3 font-weight-bold btn-md text-1">view more...</a></p>
+								</div>
+							</div>
+
+							<div class="row mt-4">
+								<div class="col-md-4 mb-3 mb-md-0">
+									<a href="prepare-for-visit.php" class="text-decoration-none">
+										<img alt="" class="img-fluid" src="img/demos/medical/gallery/g-4.jpg">
+									</a>
+								</div>
+								<div class="col-md-8">
+									<h4 class="font-weight-semibold mb-1 mt-1">
+										<a href="patients.php" class="text-decoration-none">Patients</a>
+									</h4>
+									<p class="mb-3">Our patients </p>
+									<p><a href="patients.php" class="btn btn-outline btn-quaternary custom-button text-uppercase mt-2 mb-3 font-weight-bold btn-md text-1">view more...</a></p>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+			
+		</div>
+		<?php include('assets/partials/footer.php'); ?>
+		<!-- Vendor -->
+		<script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/jquery.appear/jquery.appear.min.js"></script>
+		<script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+		<script src="vendor/jquery.cookie/jquery.cookie.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<script src="vendor/jquery.validation/jquery.validate.min.js"></script>
+		<script src="vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+		<script src="vendor/jquery.gmap/jquery.gmap.min.js"></script>
+		<script src="vendor/lazysizes/lazysizes.min.js"></script>
+		<script src="vendor/isotope/jquery.isotope.min.js"></script>
+		<script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+		<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<script src="vendor/vide/jquery.vide.min.js"></script>
+		<script src="vendor/vivus/vivus.min.js"></script>
+
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+
+		<!-- Current Page Vendor and Views -->
+		<script src="js/views/view.contact.js"></script>
+
+		<!-- Demo -->
+		<script src="js/demos/demo-medical.js"></script>
+
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+	<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"6dcb4d71fb4a845f","version":"2021.12.0","r":1,"token":"03fa3b9eb60b49789931c4694c153f9b","si":100}' crossorigin="anonymous"></script>
+</body>
+</html>
