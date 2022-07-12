36c0c86a99a0b370717f8664817dac18dedd02ab
Bibek kun <bibekrai655@gmail.com>
2022-07-12 11:28:09 +0545

    necessary resources

---
+++ assets/partials/footer.php
@@ -0,0 +1,70 @@
+
+			<footer id="footer" class="m-0">
+				<div class="container">
+					<div class="row pt-5 pb-4">
+						<div class="col-md-4 col-lg-3 mb-2">
+							<h4 class="mb-4">Location</h4>
+							<p>
+								Nalanda Tripitak<br>
+								Balaju, Kathmandu<br>
+								Nepal<br>
+								Phone : +61-404142381
+							</p>
+						</div>
+						<div class="col-md-4 col-lg-3">
+							<h4 class="mb-4">Opening Hours</h4>
+							<div class="info custom-info">
+								<span>Sun-Thu</span>
+								<span>8:30 am to 5:00 pm</span>
+							</div>
+							<div class="info custom-info">
+								<span>Friday</span>
+								<span>9:30 am to 1:00 pm</span>
+							</div>
+							<div class="info custom-info">
+								<span>Saturday</span>
+								<span>Closed</span>
+							</div>
+						</div>
+						<div class="col-md-4 col-lg-3">
+							<div class="contact-details">
+								<h4 class="mb-4">Emergency Cases</h4>
+								<a class="text-decoration-none">
+									<strong class="bold">024-591110</strong>
+									<strong class="bold">025-525555</strong>
+									
+								</a>
+							</div>
+						</div>
+						<div class="col-lg-2 text-md-center text-lg-start ms-lg-auto">
+							<h4 class="mb-4">Social Media</h4>
+							<ul class="social-icons">
+								<li class="social-icons-facebook">
+									<a href="https://www.facebook.com/" target="_blank" title="Facebook">
+										<i class="fab fa-facebook-f"></i>
+									</a>
+								</li>
+								<li class="social-icons-twitter">
+									<a href="https://www.twitter.com/" target="_blank" title="Twitter">
+										<i class="fab fa-twitter"></i>
+									</a>
+								</li>
+								<li class="social-icons-linkedin">
+									<a href="https://www.linkedin.com/" target="_blank" title="Linkedin">
+										<i class="fab fa-linkedin-in"></i>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="footer-copyright pt-3 pb-3">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-12 text-center m-0">
+								<p>Infinte IT Solutions Nepal Pvt Ltd. ©2022. All rights Reserved.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
---
+++ assets/partials/header.php
@@ -0,0 +1,125 @@
+<!-- Web Fonts  -->
+<link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CShadows+Into+Light&amp;display=swap" rel="stylesheet" type="text/css">
+
+<!-- Vendor CSS -->
+<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="vendor/animate/animate.compat.css">
+<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">
+<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
+<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
+<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
+
+<!-- Theme CSS -->
+<link rel="stylesheet" href="css/theme.css">
+<link rel="stylesheet" href="css/theme-elements.css">
+<link rel="stylesheet" href="css/theme-blog.css">
+<link rel="stylesheet" href="css/theme-shop.css">
+
+<!-- Demo CSS -->
+<link rel="stylesheet" href="css/demos/demo-medical.css">
+
+<!-- Skin CSS -->
+<link id="skinCSS" rel="stylesheet" href="css/skins/skin-medical.css">
+
+<!-- Theme Custom CSS -->
+<link rel="stylesheet" href="css/custom.css">
+
+<!-- Head Libs -->
+<script src="vendor/modernizr/modernizr.min.js"></script>
+
+
+<header id="header" class="header-effect-shrink" data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': false, 'stickyChangeLogo': true, 'stickyStartAt': 120, 'stickyHeaderContainerHeight': 70}">
+				<div class="header-body border-top-0">
+					<div class="header-top header-top-default header-top-borders border-bottom-0">
+						<div class="container h-100">
+							<div class="header-row h-100">
+								<div class="header-column justify-content-end">
+									<div class="header-row">
+										<nav class="header-nav-top">
+											<ul class="nav nav-pills">
+												<li class="nav-item nav-item-borders py-2 d-none d-sm-inline-flex">
+													<span class="ps-0"><i class="far fa-dot-circle text-4 text-color-primary" style="top: 1px;"></i> Balaju, Kathmandu</span>
+												</li>
+												<li class="nav-item nav-item-borders py-2">
+													<a href="tel:123-456-7890"><i class="fab fa-whatsapp text-4 text-color-primary" style="top: 0;"></i>+61-404142381</a>
+												</li>
+												<li class="nav-item nav-item-borders py-2 pe-1 d-none d-md-inline-flex">
+													<a href="https://www.okler.net/cdn-cgi/l/email-protection#630e020a0f23070c0e020a0d4d000c0e"><i class="far fa-envelope text-4 text-color-primary" style="top: 1px;"></i> <span class="__cf_email__" data-cfemail="9af7fbf3f6dafef5f7fbf3f4b4f9f5f7">@infonalandatripitak.com</span></a>
+												</li>
+											</ul>
+										</nav>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="header-container container">
+						<div class="header-row">
+							<div class="header-column">
+								<div class="header-row">
+									<div class="header-logo">
+										<a href="index.php">
+											<img alt="Porto" width="143" height="40" src="img/demos/business-consulting-3/logo.png">
+										</a>
+									</div>
+								</div>
+							</div>
+							<div class="header-column justify-content-end">
+								<div class="header-row">
+									<div class="header-nav order-2 order-lg-1">
+										<div class="header-nav-main header-nav-main-square header-nav-main-effect-1 header-nav-main-sub-effect-1">
+											<nav class="collapse">
+												<ul class="nav nav-pills" id="mainNav">
+													<li class="dropdown-full-color dropdown-secondary">
+														<a class="nav-link" href="index.php" >
+															Home
+														</a>
+													</li>
+													<li class="dropdown-full-color dropdown-secondary">
+														<a class="nav-link " href="about-us.php">
+															About Us
+														</a>
+													</li>
+													<li class="dropdown dropdown-full-color dropdown-secondary">
+														<a class="nav-link dropdown-toggle" class="dropdown-toggle" href="departments.php">
+															Departments
+														</a>
+														<ul class="dropdown-menu">
+															<li><a class="dropdown-item" href="Cardiology.php">Cardiology</a></li>
+															<li><a class="dropdown-item" href="Gastroenterology.php">Gastroenterology</a></li>
+															<li><a class="dropdown-item" href="Pulmonology.php">Pulmonology</a></li>
+															<li><a class="dropdown-item" href="Dental.php">Dental</a></li>
+															<li><a class="dropdown-item" href="Gynecology.php">Gynecology</a></li>
+															<li><a class="dropdown-item" href="Hepatology.php">Hepatology</a></li>
+														</ul>
+													</li>
+													<li class="dropdown-full-color dropdown-secondary">
+														<a class="nav-link" href="doctors.php">
+															Doctors
+														</a>
+													</li>
+													<li class="dropdown-full-color dropdown-secondary">
+														<a class="nav-link" href="resources.php">
+															Resources
+														</a>
+													</li>
+													</li>
+													<li class="dropdown-full-color dropdown-secondary">
+														<a class="nav-link" href="contact.php">
+															Contact
+														</a>
+													</li>
+												</ul>
+											</nav>
+										</div>
+										<button class="btn header-btn-collapse-nav" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav">
+											<i class="fas fa-bars"></i>
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
