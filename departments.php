2610a6773a09f4687db6083ec76e2f24170b4ba9
Bibekkun <bibek@infiniteitsolutionsnepal.com>
2022-07-12 11:23:05 +0545

    made changes in departments section'

---
+++ departments.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+<head>
+		
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+
+		<title>Nalanda Tripitak | Medical care provider | Departments</title>	
+
+		<meta name="keywords" content="Nalanda Tripitak | Departments" />
+		<meta name="description" content="Health Care Cente | Departments">
+		<meta name="author" content="nalandatripitak">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+		<?php include('assets/partials/header.php'); ?>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-SEP1T05Z5V"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'G-SEP1T05Z5V');
+		</script>
+
+	</head>
+	<body>
+
+		<div class="body">
+
+			
+
+			<div role="main" class="main">
+				
+				<section class="page-header page-header-modern bg-color-primary page-header-md">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-8 order-2 order-md-1 align-self-center p-static">
+								<h1>Departments</h1>
+							</div>
+							<div class="col-md-4 order-1 order-md-2 align-self-center">
+								<ul class="breadcrumb d-block text-md-end breadcrumb-light">
+									<li><a href="index.php">Home</a></li>
+									<li class="active">Departments</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<div class="container">
+
+					<div class="row mt-5 mb-5">
+						<div class="col">
+
+							<h2 class="font-weight-semibold mb-3">Health Specialists</h2>
+
+							<p class="lead font-weight-normal mb-5">At Nalanda Triptak Health Care Center, we offer a wide array of service departments to our patients.</p>
+
+							<div class="appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="100">
+								<div class="feature-box feature-box-style-2">
+									<div class="feature-box-icon" style="min-width: 4.7rem;">
+										<img src="img/demos/medical/icons/department-icon-cardiology.png" alt class="img-fluid" />
+									</div>
+									<div class="feature-box-info">
+										<h4 class="font-weight-semibold mb-1">Cardiology</h4>
+										<p class="text-bold">Our cardiology services, including cardiac rehabilitation and stress testing, are here to help you manage and treat your heart health no matter what you might need.</p>
+										<p class="mb-0"><a href="cardiology.php" class="btn btn-outline btn-quaternary custom-button text-uppercase mt-3 font-weight-bold btn-md text-1">view more...</a></p>
+									</div>	
+								</div>
+
+								<hr class="solid my-5">
+							</div>
+
+							<div class="appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="100">
+								<div class="feature-box feature-box-style-2">
+									<div class="feature-box-icon" style="min-width: 4.7rem;">
+										<img src="img/demos/medical/icons/department-icon-gastroenterology.png" alt class="img-fluid" />
+									</div>
+									<div class="feature-box-info">
+										<h4 class="font-weight-semibold mb-1">Gastroenterology</h4>
+										<p class="text-bold">Our gastroenterology service includes stomach and intestinal treatment, gastroscopy, endoscopy and colonoscopy.</p>
+										<p class="mb-0"><a href="Gastroenterology.php" class="btn btn-outline btn-quaternary custom-button text-uppercase mt-3 font-weight-bold btn-md text-1">view more...</a></p>
+									</div>
+								</div>
+
+								<hr class="solid my-5">
+							</div>
+
+							<div class="appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="100">
+								<div class="feature-box feature-box-style-2">
+									<div class="feature-box-icon" style="min-width: 4.7rem;">
+										<img src="img/demos/medical/icons/department-icon-pulmonology.png" alt class="img-fluid" />
+									</div>
+									<div class="feature-box-info">
+										<h4 class="font-weight-semibold mb-1">Pulmonology</h4>
+										<p class="text-bold">We provide consultative, diagnostic, and therapeutic services for patients with lung and breathing problems like asthma and lung cancer. </p>
+										<p class="mb-0"><a href="Pulmonology.php" class="btn btn-outline btn-quaternary custom-button text-uppercase mt-3 font-weight-bold btn-md text-1">view more...</a></p>
+									</div>
+								</div>
+
+								<hr class="solid my-5">
+							</div>
+
+							<div class="appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="100">
+								<div class="feature-box feature-box-style-2">
+									<div class="feature-box-icon" style="min-width: 4.7rem;">
+										<img src="img/demos/medical/icons/department-icon-dental.png" alt class="img-fluid" />
+									</div>
+									<div class="feature-box-info">
+										<h4 class="font-weight-semibold mb-1">Dental</h4>
+										<p class="text-bold">We provide teeth scaling and polishing, root canal treatment,clear aligners and also we put invisible braces and prevent tooth decay (cavities) and gum disease.</p><p class="mb-0"><a href="Dental.php" class="btn btn-outline btn-quaternary custom-button text-uppercase mt-3 font-weight-bold btn-md text-1">view more...</a></p>
+									</div>
+								</div>
+
+								<hr class="solid my-5">
+							</div>
+
+							<div class="appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="100">
+								<div class="feature-box feature-box-style-2">
+									<div class="feature-box-icon" style="min-width: 4.7rem;">
+										<img src="img/demos/medical/icons/department-icon-gynecology.png" alt class="img-fluid" />
+									</div>
+									<div class="feature-box-info">
+										<h4 class="font-weight-semibold mb-1">Gynecology</h4>
+										<p class="text-bold">We serve patients in adult, adolescent and pediatric gynecologic care as well as for identified problems in the areas of infertility and menopause.</p>
+										<p class="mb-0"><a href="Gynecology.php" class="btn btn-outline btn-quaternary custom-button text-uppercase mt-3 font-weight-bold btn-md text-1">view more...</a></p>
+									</div>
+								</div>
+
+								<hr class="solid my-5">
+							</div>
+
+							<div class="appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="100">
+								<div class="feature-box feature-box-style-2">
+									<div class="feature-box-icon" style="min-width: 4.7rem;">
+										<img src="img/demos/medical/icons/department-icon-hepatology.png" alt class="img-fluid" />
+									</div>
+									<div class="feature-box-info">
+										<h4 class="font-weight-semibold mb-1">Hepatology</h4>
+										<p class="text-bold">We provides management of patients with primary liver cancers and associated liver disease in the context of a multi-disciplinary framework with other medical specialties.</p>
+										<p class="mb-0"><a href="Hepatology.php" class="btn btn-outline btn-quaternary custom-button text-uppercase mt-3 font-weight-bold btn-md text-1">view more...</a></p>
+									</div>
+								</div>
+							</div>
+
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+
+			
+		</div>
+		<?php include('assets/partials/footer.php'); ?>
+		<!-- Vendor -->
+		<script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/jquery.appear/jquery.appear.min.js"></script>
+		<script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+		<script src="vendor/jquery.cookie/jquery.cookie.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<script src="vendor/jquery.validation/jquery.validate.min.js"></script>
+		<script src="vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+		<script src="vendor/jquery.gmap/jquery.gmap.min.js"></script>
+		<script src="vendor/lazysizes/lazysizes.min.js"></script>
+		<script src="vendor/isotope/jquery.isotope.min.js"></script>
+		<script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+		<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<script src="vendor/vide/jquery.vide.min.js"></script>
+		<script src="vendor/vivus/vivus.min.js"></script>
+
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+
+		<!-- Current Page Vendor and Views -->
+		<script src="js/views/view.contact.js"></script>
+
+		<!-- Demo -->
+		<script src="js/demos/demo-medical.js"></script>
+
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+	<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"6dcb4d5a48aba767","version":"2021.12.0","r":1,"token":"03fa3b9eb60b49789931c4694c153f9b","si":100}' crossorigin="anonymous"></script>
+</body>
+</html>
