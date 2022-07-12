dca4cc16c67f566e78a893e82ace9aa787261d80
Bibek kun <bibekrai655@gmail.com>
2022-07-12 13:17:28 +0545

    added doctors section

---
+++ doctors.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html>
+<head>
+		
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+
+		<title>Nalanda Tripitak | Medical care provider | Departments | Doctors</title>	
+
+		<meta name="keywords" content="Nalanda Tripitak | Doctors" />
+		<meta name="description" content="Health Care Center | Doctors">
+		<meta name="author" content="nalandatripitak">
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+		<?php include('assets/partials/header.php'); ?>
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-SEP1T05Z5V"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'G-SEP1T05Z5V');
+		</script>
+
+	</head>
+	<body>
+
+		<div class="body">
+
+			<div role="main" class="main">
+				
+				<section class="page-header page-header-modern bg-color-primary page-header-md">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-8 order-2 order-md-1 align-self-center p-static">
+								<h1>Doctors</h1>
+							</div>
+							<div class="col-md-4 order-1 order-md-2 align-self-center">
+								<ul class="breadcrumb d-block text-md-end breadcrumb-light">
+									<li><a href="index.php">Home</a></li>
+									<li class="active">Doctors</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="team">
+					<div class="container">
+
+						<div class="row mt-5 mb-5">
+							<div class="col">
+
+								<h2 class="font-weight-semibold mb-3">Meet our Specialists</h2>
+
+								<p class="lead font-weight-normal">At Nalanda Tripitak Health Care Center , we provide various skilled doctors in every department to help in your improvement of Health condition.</p>
+
+								<div id="porfolioAjaxBoxMedical" class="ajax-box ajax-box-init mb-4">
+
+									<div class="bounce-loader">
+										<div class="bounce1"></div>
+										<div class="bounce2"></div>
+										<div class="bounce3"></div>
+									</div>
+
+									<div class="ajax-box-content" id="porfolioAjaxBoxContentMedical"></div>
+
+								</div>
+
+								<ul class="nav nav-pills sort-source mb-4 pb-1" data-sort-id="portfolio" data-option-key="filter" data-plugin-options="{'layoutMode': 'fitRows', 'filter': '*'}">
+									<li class="nav-item active" data-option-value="*" class="active"><a class="nav-link active" href="#">Show All</a></li>
+									<li class="nav-item" data-option-value=".cardiology"><a class="nav-link" href="#">Cardiology</a></li>
+									<li class="nav-item" data-option-value=".gastroenterology"><a class="nav-link" href="#">Gastroenterology</a></li>
+									<li class="nav-item" data-option-value=".pulmonology"><a class="nav-link" href="#">Pulmonology</a></li>
+									<li class="nav-item" data-option-value=".dental"><a class="nav-link" href="#">Dental</a></li>
+									<li class="nav-item" data-option-value=".gynecology"><a class="nav-link" href="#">Gynecology</a></li>
+								</ul>
+
+								<div class="sort-destination-loader sort-destination-loader-showing">
+									<div class="row portfolio-list sort-destination" data-sort-id="portfolio">
+										<div class="col-md-6 col-lg-3 isotope-item cardiology">
+											<div class="portfolio-item">
+												<a href="#" data-href="demo-medical-doctors-detail" data-ajax-on-page class="text-decoration-none">
+													<span class="thumb-info thumb-info-no-zoom thumb-info-hide-wrapper-bg">
+														<span class="thumb-info-wrapper m-0">
+															<img src="img/demos/medical/doctors/doctor1.png" class="img-fluid" alt="">
+														</span>
+														<span class="thumb-info-caption p-4">
+															<span class="custom-thumb-info-title">
+																<span class="custom-thumb-info-type font-weight-light text-4">Cardiology</span>
+																<span class="custom-thumb-info-inner font-weight-semibold text-5">Dr. Bijoy G Rajbanshi</span>
+																<i class="icon-arrow-right-circle icons font-weight-semibold text-5 "></i>
+															</span>
+														</span>
+													</span>
+												</a>
+											</div>
+										</div>
+										<div class="col-md-6 col-lg-3 isotope-item gastroenterology">
+											<div class="portfolio-item">
+												<a href="#" data-href="demo-medical-doctors-detail" data-ajax-on-page class="text-decoration-none">
+													<span class="thumb-info thumb-info-no-zoom thumb-info-hide-wrapper-bg">
+														<span class="thumb-info-wrapper m-0">
+															<img src="img/demos/medical/doctors/doctor2.png" class="img-fluid" alt="">
+														</span>
+														<span class="thumb-info-caption p-4">
+															<span class="custom-thumb-info-title">
+																<span class="custom-thumb-info-type font-weight-light text-4">Gastroenterology</span>
+																<span class="custom-thumb-info-inner font-weight-semibold text-5">Prof.Dr.Sita Ram Chaudary</span>
+																<i class="icon-arrow-right-circle icons font-weight-semibold text-5 "></i>
+															</span>
+														</span>
+													</span>
+												</a>
+											</div>
+										</div>
+										<div class="col-md-6 col-lg-3 isotope-item pulmonology">
+											<div class="portfolio-item">
+												<a href="#" data-href="demo-medical-doctors-detail" data-ajax-on-page class="text-decoration-none">
+													<span class="thumb-info thumb-info-no-zoom thumb-info-hide-wrapper-bg">
+														<span class="thumb-info-wrapper m-0">
+															<img src="img/demos/medical/doctors/doctor3_1.png" class="img-fluid" alt="">
+														</span>
+														<span class="thumb-info-caption p-4">
+															<span class="custom-thumb-info-title">
+																<span class="custom-thumb-info-type font-weight-light text-4">Pulmonology</span>
+																<span class="custom-thumb-info-inner font-weight-semibold text-5">Dr.Rakshya <br>Pandey</span>
+																<i class="icon-arrow-right-circle icons font-weight-semibold text-5 "></i>
+															</span>
+														</span>
+													</span>
+												</a>
+											</div>
+										</div>
+										<div class="col-md-6 col-lg-3 isotope-item dental">
+											<div class="portfolio-item">
+												<a href="#" data-href="demo-medical-doctors-detail" data-ajax-on-page class="text-decoration-none">
+													<span class="thumb-info thumb-info-no-zoom thumb-info-hide-wrapper-bg">
+														<span class="thumb-info-wrapper m-0">
+															<img src="img/demos/medical/doctors/doctor4_3.png" class="img-fluid" alt="">
+														</span>
+														<span class="thumb-info-caption p-4">
+															<span class="custom-thumb-info-title">
+																<span class="custom-thumb-info-type font-weight-light text-4">Dental</span>
+																<span class="custom-thumb-info-inner font-weight-semibold text-5">Dr.Rajeena Pradhan</span>
+																<i class="icon-arrow-right-circle icons font-weight-semibold text-5 "></i>
+															</span>
+														</span>
+													</span>
+												</a>
+											</div>
+										</div>
+										<div class="col-md-6 col-lg-3 isotope-item gynecology">
+											<div class="portfolio-item">
+												<a href="#" data-href="demo-medical-doctors-detail" data-ajax-on-page class="text-decoration-none">
+													<span class="thumb-info thumb-info-no-zoom thumb-info-hide-wrapper-bg">
+														<span class="thumb-info-wrapper m-0">
+															<img src="img/demos/medical/doctors/doctor5.jpg" class="img-fluid" alt="">
+														</span>
+														<span class="thumb-info-caption p-4">
+															<span class="custom-thumb-info-title">
+																<span class="custom-thumb-info-type font-weight-light text-4">Gynecology</span>
+																<span class="custom-thumb-info-inner font-weight-semibold text-5">Pr.Dr.Achala<br> Vaidya</span>
+																<i class="icon-arrow-right-circle icons font-weight-semibold text-5 "></i>
+															</span>
+														</span>
+													</span>
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+
+					</div>
+				</section>
+
+			</div>
+		</div>
+		<?php include('assets/partials/footer.php'); ?>
+		<!-- Vendor -->
+		<script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/jquery.appear/jquery.appear.min.js"></script>
+		<script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+		<script src="vendor/jquery.cookie/jquery.cookie.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<script src="vendor/jquery.validation/jquery.validate.min.js"></script>
+		<script src="vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+		<script src="vendor/jquery.gmap/jquery.gmap.min.js"></script>
+		<script src="vendor/lazysizes/lazysizes.min.js"></script>
+		<script src="vendor/isotope/jquery.isotope.min.js"></script>
+		<script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+		<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<script src="vendor/vide/jquery.vide.min.js"></script>
+		<script src="vendor/vivus/vivus.min.js"></script>
+
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+
+		<!-- Current Page Vendor and Views -->
+		<script src="js/views/view.contact.js"></script>
+
+		<!-- Demo -->
+		<script src="js/demos/demo-medical.js"></script>
+
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+	<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"6dcb4d698c47a767","version":"2021.12.0","r":1,"token":"03fa3b9eb60b49789931c4694c153f9b","si":100}' crossorigin="anonymous"></script>
+</body>
+</html>
